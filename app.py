e81c7c0f7e7f19e57ed531be7e0cf1549a593ced
chaodreaming <645121710@qq.com>
2024-03-12 16:33:10 +0800

    Annotation jump to latexlive

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
-import execjs
-
 from PIL import Image
 import streamlit
 from simple_latex_ocr.models import Latex_OCR
-
-js_code = """
-function encodeToBase64(latex) {
-    var str = encodeURI(latex); // 进行URL编码
-    str = btoa(str); // 进行Base64编码
-    return str;
-}
-"""
-ctx = execjs.compile(js_code)
+# import execjs
+# js_code = """
+# function encodeToBase64(latex) {
+#     var str = encodeURI(latex); // URL
+#     str = btoa(str); // Base64
+#     return str;
+# }
+# """
+# ctx = execjs.compile(js_code)
 model = Latex_OCR()
 if __name__ == '__main__':
     streamlit.set_page_config(page_title='Simple-LaTeX-OCR')
@@ -36,11 +34,11 @@ if __name__ == '__main__':
                 response = model.predict(uploaded_file.getvalue())
             if "formula" in response:
                 latex_code = response["formula"]
-                latex_encode = ctx.call("encodeToBase64", latex_code)
+                # latex_encode = ctx.call("encodeToBase64", latex_code)
                 streamlit.code(latex_code, language='latex')
                 streamlit.markdown(f'$\\displaystyle {latex_code}$')
-                link_html = f'<a href="https://www.latexlive.com/#{latex_encode}" target="_blank"><button style="color: black;">Online Editing</button></a>'
-                streamlit.markdown(link_html, unsafe_allow_html=True)
+                # link_html = f'<a href="https://www.latexlive.com/#{latex_encode}" target="_blank"><button style="color: black;">Online Editing</button></a>'
+                # streamlit.markdown(link_html, unsafe_allow_html=True)
             else:
                 streamlit.error(response)
         else:
